479f413beee88923b9b0bf9da18d559a5718f693
worldOfRavi <rajendra20023@gmail.com>
2025-02-07 00:11:11 -0500

    load more product project is done

--- src/component/Load-more/index.jsx
+++ src/component/Load-more/index.jsx
@@ -4,6 +4,7 @@ function LoadMore() {
   const [imageList, setImageList] = useState([]);
   const [laoding, setLoading] = useState(false);
   const [count, setCount] = useState(0);
+  const [buttonDisable, setButtonDisable] = useState(false)
   const fetchImages = async () => {
     try {
       setLoading(true);
@@ -24,7 +25,10 @@ function LoadMore() {
   useEffect(() => {
     fetchImages();
   }, [count]);
-  console.log(imageList);
+
+  useEffect(()=>{
+    if(imageList.length === 100) setButtonDisable(true)
+  },[imageList])
 
   if (laoding) {
     return <p>Loading please wait...!</p>;
@@ -33,22 +37,28 @@ function LoadMore() {
     <React.Fragment>
       <div className="flex flex-wrap gap-4 mt-4 justify-center">
         {imageList && imageList.length > 0
-          ? imageList.map((item, index) => (
+          ? imageList.map((item,index) => (
+              <div className=" border border-slate-400">
               <div
-                className="w-[300px] h-[250px] border border-slate-400"
+                className="w-[300px] h-[250px]"
                 key={index}
               >
                 <img
                   src={item.images[0]}
                   alt="random image"
-                  className="w-full h-full object-cover p-3"
+                  className="w-full h-full p-3"
                 />
+              </div>
+                <p className="text-center">{item.title}</p>
               </div>
             ))
           : null}
       </div>
       <div className="flex justify-center">
-        <button onClick={()=>setCount(count+1)} className="border py-2 px-4 bg-slate-400 my-4 text-white">Load More</button>
+        <button disabled={buttonDisable} onClick={()=>setCount(count+1)} className="border py-2 px-4 bg-slate-400 my-4 text-white">Load More</button>
+        {
+            buttonDisable && <p>You reached the 100 product limit</p>
+        }
       </div>
     </React.Fragment>
   );
